7569532f72fe0a4907c3dea02ba0273944d80e0b
EdithCerdaMonroyHackademy <98177408+EdithCerdaMonroyHackademy@users.noreply.github.com>
2022-02-13 22:18:58 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const ejercicio1 = [
+    3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
+    19, 88, 456, 13, 23, 24,
+  ];
+for (let i = 0; i < ejercicio1.length; i++) {
+    if (i % i != 0) {
+        console.log(`${ejercicio1[i]}`);
+      }
+    
+}
